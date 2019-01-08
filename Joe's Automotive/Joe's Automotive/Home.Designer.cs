6f78762d56ea9708522fbe3d0e88b89a36ec2313
Zachary Seebeck <seebeckz@live.com>
2019-01-08 11:02:36 -0500

    Update Home.Designer.cs

--- Joe's Automotive/Joe's Automotive/Home.Designer.cs
+++ Joe's Automotive/Joe's Automotive/Home.Designer.cs
@@ -31,13 +31,17 @@
             this.panelSidebar = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panelHeader = new System.Windows.Forms.Panel();
-            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelHome = new System.Windows.Forms.Panel();
+            this.lblCompName = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
             this.panelSidebar.SuspendLayout();
+            this.panelHome.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelSidebar
             // 
             this.panelSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(41)))), ((int)(((byte)(81)))));
+            this.panelSidebar.Controls.Add(this.lblCompName);
             this.panelSidebar.Controls.Add(this.panel3);
             this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelSidebar.Location = new System.Drawing.Point(0, 0);
@@ -58,30 +62,54 @@
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(200, 0);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(914, 76);
+            this.panelHeader.Size = new System.Drawing.Size(914, 50);
             this.panelHeader.TabIndex = 1;
             // 
-            // panelContent
+            // panelHome
             // 
-            this.panelContent.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(200, 76);
-            this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(914, 515);
-            this.panelContent.TabIndex = 0;
+            this.panelHome.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panelHome.Controls.Add(this.lbl);
+            this.panelHome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelHome.Location = new System.Drawing.Point(200, 50);
+            this.panelHome.Name = "panelHome";
+            this.panelHome.Size = new System.Drawing.Size(914, 541);
+            this.panelHome.TabIndex = 0;
+            // 
+            // lblCompName
+            // 
+            this.lblCompName.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompName.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCompName.Location = new System.Drawing.Point(12, 9);
+            this.lblCompName.Name = "lblCompName";
+            this.lblCompName.Size = new System.Drawing.Size(179, 83);
+            this.lblCompName.TabIndex = 3;
+            this.lblCompName.Text = "Joe\'s \r\nAutomotive\r\n";
+            this.lblCompName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl
+            // 
+            this.lbl.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbl.Location = new System.Drawing.Point(6, 3);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(902, 39);
+            this.lbl.TabIndex = 4;
+            this.lbl.Text = "Welcome! Let\'s get started.";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1114, 591);
-            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelHome);
             this.Controls.Add(this.panelHeader);
             this.Controls.Add(this.panelSidebar);
             this.MinimumSize = new System.Drawing.Size(1130, 630);
             this.Name = "Home";
             this.Text = "Joe\'s Automotive - Home";
             this.panelSidebar.ResumeLayout(false);
+            this.panelHome.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -91,7 +119,9 @@
         private System.Windows.Forms.Panel panelSidebar;
         private System.Windows.Forms.Panel panelHeader;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Panel panelHome;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblCompName;
     }
 }
 
