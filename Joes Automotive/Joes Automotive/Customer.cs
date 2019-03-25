fec3433ecb19b2d2e08ce813965da14fa67261c2
Zachary Seebeck <seebeckz@live.com>
2019-03-25 10:22:02 -0400

    Bullshit

--- Joes Automotive/Joes Automotive/Customer.cs
+++ Joes Automotive/Joes Automotive/Customer.cs
@@ -30,10 +30,13 @@ namespace Joes_Automotive
 
         private void Customer_Load(object sender, EventArgs e)
         {
+            bindingNavigatorPositionItem.Focus();
             // TODO: This line of code loads data into the 'joesBigBoyDatabaseDataSet.Customers' table. You can move, or remove it, as needed.
             this.customersTableAdapter.Fill(this.joesBigBoyDatabaseDataSet.Customers);
             bindingNavigatorPositionItem.Text = currentCustomerID.ToString();
             customersBindingNavigator.Update();
+            this.tableAdapterManager.UpdateAll(this.joesBigBoyDatabaseDataSet);
+
 
         }
 
